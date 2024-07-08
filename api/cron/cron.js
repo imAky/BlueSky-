a030e59e8687e4b570099976fd83d8574f5ff45e
imAky <aky@example.com>
2024-07-08 16:22:21 +0530

    update-about

--- api/cron/cron.js
+++ api/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://social-connect-4u5k.onrender.com";
+const URL = "https://bluesky-xk0s.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
